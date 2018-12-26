9bb6eb38713a4b8ff5932fb3086838d2d9e31457
Yumekuii <kingdomhwang@gmail.com>
2018-12-27 00:44:49 +0800

    add some function

--- js/shoppingCart.js
+++ js/shoppingCart.js
@@ -5,3 +5,36 @@ $.getJSON("../data/breakfastType.json")
  .then( function( json ){
               breakfastType = json ;
           } );
+
+function isChecked( id ){
+    id += ":checked" ;
+    if ( $( id ).val() === "on" )
+    {
+      return true;
+    }
+    if ( $( id ).val() === undefined )
+    {
+      return false;
+    }
+  	else
+    {
+      return id;
+    }
+}
+
+function updateMenu(){
+    var length ;
+    if ( isChecked( "#chinese" ) )
+    {
+      length = breakfastType.chinese.content.length ;
+
+    }
+    else if ( isChecked( "#british" ) )
+    {
+
+    }
+    else if ( isChecked( "#french" ) )
+    {
+
+    }
+}
