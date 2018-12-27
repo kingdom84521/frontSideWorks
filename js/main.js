5ae0fa167e799e57c669658adeab6b150156f7ad
Yumekuii <kingdomhwang@gmail.com>
2018-12-27 16:12:42 +0800

    unitprice, quantity, subtotal auto change finished

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
 import { changeColorButtonAction } from './changeColor.js' ;
 import { calculatorButtonAction } from './calculator.js' ;
+import { shoppingCartAllAction } from './shoppingCart.js' ;
 
 $( document ).ready(
   function(){
     $("#changeColor").load("../html/changeColor.html", changeColorButtonAction);
     $("#calculator").load("../html/calculator.html", calculatorButtonAction);
-    $("#breakfastShoppingCart").load("../html/shoppingCart.html");
+    $("#breakfastShoppingCart").load("../html/shoppingCart.html", shoppingCartAllAction);
   }
 )
--- js/shoppingCart.js
+++ js/shoppingCart.js
@@ -1,40 +1,94 @@
 var breakfastType = null ;
 
-//store json to variable
-$.getJSON("../data/breakfastType.json")
- .then( function( json ){
-              breakfastType = json ;
-          } );
-
 function isChecked( id ){
     id += ":checked" ;
     if ( $( id ).val() === "on" )
     {
-      return true;
+        return true;
     }
     if ( $( id ).val() === undefined )
     {
-      return false;
+        return false;
     }
   	else
     {
-      return id;
+        return id;
     }
 }
 
 function updateMenu(){
     var length ;
+    var typeNum ;
+    var breakfastStore = breakfastType.chinese.content ;
+
+    $("#menu option").remove() ;
+
     if ( isChecked( "#chinese" ) )
     {
-      length = breakfastType.chinese.content.length ;
-
+        breakfastStore = breakfastType.chinese.content ;
+        typeNum        = breakfastType.chinese.type ;
     }
     else if ( isChecked( "#british" ) )
     {
-
+        breakfastStore = breakfastType.british.content ;
+        typeNum        = breakfastType.british.type ;
     }
     else if ( isChecked( "#french" ) )
     {
+        breakfastStore = breakfastType.french.content ;
+        typeNum        = breakfastType.french.type ;
+    }
 
+    for ( var i in breakfastStore )
+    {
+        var nowName   = typeNum.toString() + breakfastStore[i].id.toString() + "->" + breakfastStore[i].name ;
+        var nowOption = new Option( nowName, breakfastStore[i].price ) ;
+
+        $("#menu").append( nowOption );
+    }
+
+    setUnitPrice() ;
+    setSubtotal() ;
+}
+
+function setUnitPrice()
+{
+    $("#unitPrice").val( $("#menu").find(":selected").val() ) ;
+    $("#quantity").val("0");
+
+    setSubtotal() ;
+}
+
+function setSubtotal()
+{
+    $("#subtotal").val( parseInt( $("#unitPrice").val() ) * parseInt( $("#quantity").val() ) ) ;
+    if ( $("#subtotal").val() === "NaN" )
+    {
+        $("#subtotal").val("0");
     }
 }
+
+function shoppingCartAllAction()
+{
+    $.getJSON('../data/breakfastType.json')
+      .then(( json ) => {
+          breakfastType = json;
+      })
+      .then(() => {
+          updateMenu();
+      })
+
+    $("#menu").change( setUnitPrice );
+    $("#british").click( updateMenu ) ;
+    $("#quantity").on( "input", setSubtotal ) ;
+}
+
+// $.getJSON("../data/breakfastType.json")
+//  .then( function( json ){
+//               breakfastType = json ;
+//
+//               updateMenu() ;
+//           } );
+
+
+export { shoppingCartAllAction } ;
